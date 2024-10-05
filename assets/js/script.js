ea3165e51503e89c318a0fedf78b607a213df1b0
Mikosy <miibewuba@gmail.com>
2024-10-05 09:05:51 +0100

    update on index page

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+// $(document).ready(function () {
+//     $(".owl-carousel").owlCarousel({
+//         loop: true, // Allows infinite loop sliding
+//         margin: 10, // Space between items
+//         nav: true,  // Show next and prev buttons
+//         autoplay: true, // Automatically start sliding
+//         autoplayTimeout: 3000, // 3 seconds delay for autoplay
+//         responsive: {
+//             0: {
+//                 items: 1 // Show 1 item on screens less than 600px
+//             },
+//             600: {
+//                 items: 2 // Show 2 items on screens 600px and up
+//             },
+//             1000: {
+//                 items: 3 // Show 3 items on screens 1000px and up
+//             }
+//         }
+//     });
+// });
